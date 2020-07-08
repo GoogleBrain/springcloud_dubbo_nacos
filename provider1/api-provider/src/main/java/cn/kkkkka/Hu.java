6e34f6bd6591870c39879e2a717fbbac7f2ee02f
googlebrain@163.com <googlebrain@163.com>
2020-07-08 17:10:14 +0800

    springcloud+dubbo+nacos

--- provider1/api-provider/src/main/java/cn/kkkkka/Hu.java
+++ provider1/api-provider/src/main/java/cn/kkkkka/Hu.java
@@ -5,6 +5,9 @@ import org.apache.dubbo.config.annotation.Service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 
+/**
+ * 配置数据动态刷新
+ */
 @RefreshScope
 @Service
 public class Hu implements studentInter {
